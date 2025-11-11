33321b6a37d408239fac9e5d6fe49306b3c0eeb7
Qzjb <zhaojianbo54@gmail.com>
2025-11-11 20:54:02 +0800

    first commit

---
+++ train.sh
@@ -0,0 +1,46 @@
+
+<<<<<<< HEAD
+# CRFV fever 2-way
+=======
+# CRFV fever 2-way 
+>>>>>>> aca5539 (first commit)
+CUDA_VISIBLE_DEVICES="0" python train_CRFV_fever_2way.py \
+--seed 1234 \
+--batch_size 16 \
+--lr 2e-5 \
+--epochs 10 \
+--weight_decay 2e-3 \
+--evi_num 5 \
+--max_seq_length 128 
+
+<<<<<<< HEAD
+# CRFV politihop 3-way
+=======
+# CRFV politihop 3-way 
+>>>>>>> aca5539 (first commit)
+CUDA_VISIBLE_DEVICES="0" python train_CRFV_politihop_3way.py \
+--seed 1234 \
+--batch_size 4 \
+--lr 1e-5 \
+--epochs 5 \
+<<<<<<< HEAD
+--weight_decay 5e-4 \
+--evi_num 20 \
+--max_seq_length 128 
+
+# CRFV politihop 2-way
+=======
+--weight_decay 3e-3 \
+--evi_num 20 \
+--max_seq_length 128 
+
+# CRFV politihop 2-way 
+>>>>>>> aca5539 (first commit)
+CUDA_VISIBLE_DEVICES="0" python train_CRFV_politihop_2way.py \
+--seed 1234 \
+--batch_size 4 \
+--lr 1e-5 \
+--epochs 10 \
+--weight_decay 5e-4 \
+--evi_num 20 \
+--max_seq_length 128 
